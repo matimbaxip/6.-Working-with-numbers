7320bca404c95f4b891d05b58d52eea9ef45116d
Dell-User <Dell-User@DESKTOP-4LNHG9G>
2023-10-09 12:30:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.Working_with_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { // you can just print numbers as below
+            Console.WriteLine(7);
+                // You can also use operators + * /
+                Console.WriteLine(6+7);
+            // modulo operators to get the remaining 
+            Console.WriteLine( 8%3);
+            // Bodmas will applies, 4+2+3 use the brackets
+            Console.WriteLine(4+2+3);
+            // whatever you do with integers you get an integr back
+            Console.WriteLine(5/3); // you get 2 not 2,5
+            // if you use a decimal and an integer you get a doube
+            Console.WriteLine(5/2.0); // you get 2,5
+            // Can also give a number to a variable
+            int num = 6;
+            //increment or decrement by 1
+            num++;
+             
+            Console.WriteLine(num);
+            //calling eg math method ,abs means absolute
+            //  you will always get the positive number
+            Console.WriteLine(Math.Abs(-40));
+            // to raise to powers
+            Console.WriteLine(Math.Pow(2,2));
+
+
+
+        }
+    }
+}
